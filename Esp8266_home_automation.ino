a8aa50446d6f7ea35250bd1f728d825c43c84a41
Siddhesh Naik <65162229+siddhesh010@users.noreply.github.com>
2021-04-25 21:36:18 +0530

    Add files via upload

---
+++ Esp8266_home_automation.ino
@@ -0,0 +1,75 @@
+/*Created By Siddhesh Naik
+  All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+
+ * * Redistributions of source code must retain the above copyright notice, this
+     list of conditions and the following disclaimer.
+
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+     list of conditions and the following disclaimer in the documentation and/or
+     other materials provided with the distribution.
+*/
+
+
+/* Create a WiFi access point and provide a web server on it. */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+#ifndef APSSID
+#define APSSID " Home Automation "  /* Set these to your desired credentials. */
+#define APPSK  "Jarvis2021"
+
+
+
+const char *ssid = APSSID;
+const char *password = APPSK;
+
+ESP8266WebServer server(80);
+
+/* Just a little test message.  Go to http://192.168.4.1 in a web browser
+   connected to this access point to see it.
+*/
+void handleRoot() {
+  server.send(200, "text/html", "");
+}
+
+void setup() {
+  pinMode(4,OUTPUT); //
+   pinMode(0,OUTPUT);
+  delay(1000);
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+  digitalWrite(4,LOW);
+  digitalWrite(0,LOW);
+}
+
+
+
+void loop() {
+server.handleClient();
+
+digitalWrite(4,server.arg("led1").toInt());
+digitalWrite(0,server.arg("led2").toInt());
+
+  
+  
+  
+ 
+}
+
+  
+  
